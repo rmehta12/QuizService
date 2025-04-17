b6b7160d7e15e4f7e443f55d4316b4af62aae925
BlueNinja7 <rajmehta0602@gmail.com>
2025-04-16 23:37:00 -0400

    Microservices Communication commit

---
+++ src/main/java/com/quiz/QuizService/services/QuestionClient.java
@@ -0,0 +1,16 @@
+package com.quiz.QuizService.services;
+
+import com.quiz.QuizService.entities.Question;
+import org.aspectj.weaver.patterns.TypePatternQuestions;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(url = "http://localhost:8082", value = "Question-Client")
+public interface QuestionClient {
+
+    @GetMapping("/question/quiz/{quizId}")
+    List<Question> getQuestionByQuiz(@PathVariable Long quizId);
+}
--- src/main/java/com/quiz/QuizService/services/impl/QuizServiceImpl.java
+++ src/main/java/com/quiz/QuizService/services/impl/QuizServiceImpl.java
@@ -2,18 +2,23 @@ package com.quiz.QuizService.services.impl;
 
 import com.quiz.QuizService.entities.Quiz;
 import com.quiz.QuizService.repositories.QuizRepository;
+import com.quiz.QuizService.services.QuestionClient;
 import com.quiz.QuizService.services.QuizService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class QuizServiceImpl implements QuizService {
 
     private QuizRepository quizRepository;
 
-    public QuizServiceImpl(QuizRepository quizRepository) {
+    private QuestionClient questionClient;
+
+    public QuizServiceImpl(QuizRepository quizRepository, QuestionClient questionClient) {
         this.quizRepository = quizRepository;
+        this.questionClient = questionClient;
     }
 
     @Override
@@ -23,11 +28,20 @@ public class QuizServiceImpl implements QuizService {
 
     @Override
     public List<Quiz> get() {
-        return quizRepository.findAll();
+        List<Quiz> quizzes = quizRepository.findAll();
+
+        List <Quiz> newQuizList = quizzes.stream().map(quiz -> {
+            quiz.setQuestions(questionClient.getQuestionByQuiz(quiz.getId()));
+            return quiz;
+        }).collect(Collectors.toList());
+
+        return newQuizList;
     }
 
     @Override
     public Quiz get(Long id) {
-        return quizRepository.findById(id).orElseThrow(() -> new RuntimeException("Quiz not found"));
+        Quiz quiz =  quizRepository.findById(id).orElseThrow(() -> new RuntimeException("Quiz not found"));
+        quiz.setQuestions(questionClient.getQuestionByQuiz(quiz.getId()));
+        return quiz;
     }
 }
