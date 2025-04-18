4ee4536bb2738d0faa0e325986993bc115fd808e
BlueNinja7 <rajmehta0602@gmail.com>
2025-04-17 21:22:22 -0400

    Final commit

--- src/main/java/com/quiz/QuizService/services/QuestionClient.java
+++ src/main/java/com/quiz/QuizService/services/QuestionClient.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(url = "http://localhost:8082", value = "Question-Client")
+//@FeignClient(url = "http://localhost:8082", value = "Question-Client")
+@FeignClient(name = "QuestionService", value = "QuestionService")
 public interface QuestionClient {
 
     @GetMapping("/question/quiz/{quizId}")
